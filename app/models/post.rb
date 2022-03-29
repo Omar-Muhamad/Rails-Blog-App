73987d1f55ebf810265d5dfae174b691e0ed51b7
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-03-30 00:08:00 +0200

    Add required methods to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
   belongs_to :user
+
+  def update_posts_counter
+    user.update(posts_counter: user.posts.count)
+  end
+
+  def last_five_comments
+    comments.last(5)
+  end
 end
