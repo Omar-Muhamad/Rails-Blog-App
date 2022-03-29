f8cd5c46159d3b2b97c0cde82419ea95afd5f145
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-03-29 23:02:26 +0200

    Correct database model

--- db/schema.rb
+++ db/schema.rb
@@ -10,35 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_24_214740) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_29_205920) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.bigint "posts_id", null: false
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.integer "author_id"
-    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "post_id", null: false
     t.bigint "user_id", null: false
-    t.bigint "posts_id", null: false
-    t.index ["posts_id"], name: "index_likes_on_posts_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
-    t.integer "author_id"
     t.string "title"
     t.string "text"
     t.integer "comments_counter"
@@ -58,9 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_24_214740) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", column: "posts_id"
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "likes", "posts", column: "posts_id"
+  add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "users"
 end
