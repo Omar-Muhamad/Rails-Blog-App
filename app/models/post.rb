9d02e1c3ed33ebd8c308e630600d67ea6a0e85b4
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-03-30 17:00:13 +0200

    Fix author_id problem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
-  belongs_to :user, foreign_key: :author_id
+  belongs_to :author, foreign_key: :author_id, class_name:"User"
 
   after_save :update_posts_counter
   def update_posts_counter
