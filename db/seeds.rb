cd886da82c2fdbe74cc3911d2c2e5769c1f0740c
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-03-30 21:52:05 +0200

    Add data seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Comment.destroy_all
+Post.destroy_all
+User.destroy_all
+
+users = []
+6.times do |index|
+  user = User.create(name: "User #{index}", photo:'https://www.pngkit.com/png/full/281-2812821_user-account-management-logo-user-icon-png.png', bio: "This is user #{index}")
+  users.push(user)
+end
+
+p "#{User.count} users created."
+
+posts = []
+6.times do |index|
+  3.times do |index2|
+    post = Post.create(author: users[index], title: "Post #{index2}", text: "This is post number #{index2}")
+    posts.push(post)
+  end
+end
+
+p "#{Post.count} posts created."
+
+comments = 6.times do |index|
+  3.times do |index2|
+    Comment.create(post: posts[index], author: users[index], text: "Hi There, this is comment number #{index2}." )
+  end
+end
+
+p "#{Comment.count} Comments created."
