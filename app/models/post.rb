6df23c0511b77b081b1310efad33857c249f0b50
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-03-30 17:07:14 +0200

    Fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   has_many :comments
   has_many :likes
-  belongs_to :author, foreign_key: :author_id, class_name:"User"
+  belongs_to :author, foreign_key: :author_id, class_name: 'User'
 
   after_save :update_posts_counter
   def update_posts_counter
