4d4e8e5aba66b86dd77a8b6fb6316b4dbad583fd
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-04-01 01:33:56 +0200

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,11 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts
-    @comments = @user.comments
   end
 
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find_by_id(params[:id])
-    @comments = @post.last_five_comments
+    @comments = @post.comments
   end
 end
