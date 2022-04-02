b1592bc9ecb8b2dbde8d1dd160464b852c24538a
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-04-02 16:12:40 +0200

    Add unit specs for user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   has_many :likes
   belongs_to :author, class_name: 'User'
 
-  validates :title, presence: true, 
+  validates :title, presence: true 
   validates :title, length: { maximum: 250 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
