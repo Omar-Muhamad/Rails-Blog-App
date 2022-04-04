47e8e71f56a6eccfbb58f99759514556e982b214
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-04-04 22:38:11 +0200

    Add devise with its views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
-    @users = User.all
+    @users = current_user
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  devise_for :users
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new]
   end
