6c70c84a85e5917fb47b87ac630d71338aa13777
Santiago Velosa <santiagovelosa@gmail.com>
2022-04-05 19:03:35 -0500

    Add cascade to likes and comments

---
+++ db/migrate/20220406000109_update_foreign_key.rb
@@ -0,0 +1,8 @@
+class UpdateForeignKey < ActiveRecord::Migration[7.0]
+  def change
+    remove_foreign_key :likes, :posts
+    remove_foreign_key :comments, :posts
+    add_foreign_key :likes, :posts, on_delete: :cascade
+    add_foreign_key :comments, :posts, on_delete: :cascade
+  end
+end
