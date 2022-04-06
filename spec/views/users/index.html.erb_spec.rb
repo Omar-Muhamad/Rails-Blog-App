2e1a7187c6262126a153cb008aae2ebe8fd39e69
Omar Muhammad <68796232+Omar-Muhamad@users.noreply.github.com>
2022-04-07 01:32:35 +0200

    Fix linters errors

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe 'users/index', type: :system, js: true do
   describe 'users index page' do
     before :each do
-      user_1 = @user = User.new(name: 'Santiago', photo: 'http://photo.com', bio: 'This is me', posts_counter: 2,
-                                email: 'santiagovelosa@gmail.com', password: '123456')
-      user_1.skip_confirmation!
-      user_1.save!
-      user_2 = @user = User.new(name: 'Omar', photo: 'http://photo.com', bio: 'This is me Omar!', posts_counter: 2,
-                                email: 'eng.omarmuhammad@gmail.com', password: 'asd123')
-      user_2.skip_confirmation!
-      user_2.save!
+      user1 = @user = User.new(name: 'Santiago', photo: 'http://photo.com', bio: 'This is me', posts_counter: 2,
+                               email: 'santiagovelosa@gmail.com', password: '123456')
+      user1.skip_confirmation!
+      user1.save!
+      user2 = @user = User.new(name: 'Omar', photo: 'http://photo.com', bio: 'This is me Omar!', posts_counter: 2,
+                               email: 'eng.omarmuhammad@gmail.com', password: 'asd123')
+      user2.skip_confirmation!
+      user2.save!
       visit user_session_path
 
       fill_in 'Email', with: 'santiagovelosa@gmail.com'
